75d128490fdba8246aa5e241a4a0e6ea66165344
sethpitts22@gmail.com <sethpitts22@gmail.com>
2018-05-18 08:00:59 -0400

    Updated to capture node info

--- generate_site_list.py
+++ generate_site_list.py
@@ -3,8 +3,8 @@ from collections import defaultdict, namedtuple
 
 # get NDB and NDC data from sas
 
-ndb_site_data = pd.read_sas(r'G:\NIDADSC\spitts\SAS_Projects\NDB\siteinfo.sas7bdat', format='sas7bdat')
-ndc_site_data = pd.read_sas(r'G:\NIDADSC\spitts\SAS_Projects\NDC\siteinfo.sas7bdat', format='sas7bdat')
+ndb_site_data = pd.read_sas('siteprot.sas7bdat', format='sas7bdat')
+ndc_site_data = pd.read_sas('siteprot.sas7bdat', format='sas7bdat')
 
 platforms = [ndb_site_data, ndc_site_data]
 
@@ -14,18 +14,19 @@ protocol_node_info = defaultdict(dict)
 all_site_info = defaultdict(dict)
 all_node_info = defaultdict(dict)
 
-site_info_tuple = namedtuple('Site_Info', ['PROT', 'SITE', 'CLOSED', 'NODE'])
-
+site_info_tuple = namedtuple('Site_Info', ['prot', 'site', 'status', 'node'])
+collection_of_sites = []
 for platform in platforms:
+    print(platform.keys())
     for index, row in platform.iterrows():
         prot = row.PROT.decode('utf-8')
-        site = row.SITENAME.decode('utf-8')
+        site = row.SITECODE.decode('utf-8')
         closed = row.CLOSED
         if closed == 0.0:
             closed = "A"
         else:
             closed = "C"
-        node = row.Node_name
+        node = row.SITECODE
         if type(node) == bytes:
             node = node.decode('utf-8')
         else:
@@ -41,42 +42,44 @@ for platform in platforms:
         else:
             protocol_node_info[prot][node] = 1
 
-        if all_node_info[prot].get(node) is not None:
-            all_node_info[prot][node] += 1
-        else:
-            all_node_info[prot][node] = 1
-
-        all_site_info[prot][site] = (closed, node)
+        collection_of_sites.append(site_info_tuple(prot, site, closed, node))
 
 protocol_info = defaultdict(dict)
 protocol_and_site_info = []
+all_prots = protocol_node_info.keys()
+print(all_prots)
+print(collection_of_sites)
 for prot, node in protocol_node_info.items():
-    print(prot, node)
     protocol_info[prot]['CTN-NODES'] = len(node.keys())
 
+protocol_and_site_info.append(['CTN-NODES'] + [protocol_info[prot]['CTN-NODES']
+                              for prot in protocol_info.keys()])
+
+print(protocol_and_site_info)
 print('-------------------------------')
+
 for prot, site in protocol_site_info.items():
-    print(prot, site)
     protocol_info[prot]['CTN-SITES'] = sum(site.values())
 
-for prot, info in protocol_site_info.items():
-    protocol_and_site_info.append(['CTN-NODES'] + protocol_info[prot]['CTN-NODES'].values())
-    protocol_and_site_info.append(['CTN-SITES'] + protocol_info[prot]['CTN-SITES'].values())
+protocol_and_site_info.append(['CTN-SITES'] + [protocol_info[prot]['CTN-SITES']
+                              for prot in protocol_info.keys()])
+print(protocol_and_site_info)
 
-just_node_info = defaultdict(dict)
-for prot, node_info in all_node_info:
-    node_name, values = node_info.items()
-    just_node_info[node_name][prot] = sum(values)
-just_site_info = defaultdict(dict)
-for prot, site_info in all_site_info:
-    site_name, values = site_info.items()
-    just_site_info[site_name][prot] =
+final_node_info = defaultdict(dict)
+for site in collection_of_sites:
+    prot = site.prot
+    site_name = site.site
+    closed = site.status
+    node = site.node
+    final_node_info[node][prot] = defaultdict(dict)
+    final_node_info[node][prot][site_name] = closed
 
+print(final_node_info)
+print("_____________________________")
+# for key, value in all_site_info.items():
+#     print(key, value, 'site')
 
-for node, node_info in just_node_info.items():
-    protocol_and_site_info.append([node] + node_info.values())
-    for
-
+node = [['NODE'] + ['NODES IN PROT']]
 
 protocol_info_df = pd.DataFrame.from_dict(protocol_info)
 all_node_info_df = pd.DataFrame.from_dict(all_node_info)
